2927c2f169642b7b83a63cb0800c8464bd9c8029
Guillermo Menéndez <gmenende@amazon.com>
2019-06-24 12:07:42 +0200

    Added CORS headers, fixed mock values escaping

--- datathon-microservice.py
+++ datathon-microservice.py
@@ -1,16 +1,24 @@
 import boto3
 import json
+import logging
 
-print('Loading function')
+## Logging setup
+logger=logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logger.info('Loading function')
 dynamo = boto3.client('dynamodb')
 
-
 def respond(err=None, res=None):
+    logger.info('Sending response err %s res %s' % (err,json.dumps(res, indent=2)) )
     return {
         'statusCode': '400' if err else '200',
         'body': err if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Methods':'DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT',
+            'Access-Control-Allow-Origin':'*'
         },
     }
 
@@ -43,32 +51,41 @@ def lambda_handler(event, context):
     }
 
     '''
-    print("Received event: " + json.dumps(event, indent=2))
+    logger.info("Received event: " + json.dumps(event, indent=2))
 
     operation = event['httpMethod']
     resource = event['resource']
+    
+    logger.debug('Operation %s - resource %s' % (operation,resource) )
 
     if operation == 'GET':
         if resource == '/challenges':
+            logger.info('get_challenges')
             return get_challenges(event)
         elif resource == '/challenges/{id}/hint':
+            logger.info('get_challenge_hint')
             return get_challenge_hint(event)
         elif resource == '/leaderboard':
+            logger.info('get_leaderboard')
             return get_leaderboard(event)
         else:
+            logger.error('No matching GET resource')
             respond(ValueError('Unknown resource "{}"'.format(resource)))
 
     elif operation == 'POST':
         if resource == '/challenges/{id}/answer':
+            logger.info('post_challenge_answer')
             return post_challenge_answer(event)
         else:
+            logger.error('No matching POST resource')
             respond(ValueError('Unknown resource "{}"'.format(resource)))
 
     else:
+        logger.error('No matching method')
         respond(ValueError('Unsupported method "{}"'.format(operation)))
 
 def get_challenges(event):
-    return respond(res=json.dumps([
+    return respond(res=[
         {
             "challengeId": "ch01",
             "title": "Challenge 1",
@@ -89,15 +106,17 @@ def get_challenges(event):
             "status": "ANSWERED",
             "answer": "This is an answer for the challenge"
         }
-    ]))
+    ])
 
 def get_challenge_hint(event):
-    respond(res=json.dumps({
+    return respond(res=
+        {
             "hint" : "This is a hint for the challenge"
-        }))
+        }
+    )
     
 def get_leaderboard(event):
-    respond(res=json.dumps([
+    return respond(res=[
         {
             "position": 1,
             "teamName": "team 1",
@@ -117,7 +136,8 @@ def get_leaderboard(event):
             "position": 4,
             "teamName": "team 4",
             "score": 100
-        }]))
+        }]
+    )
     
 def post_challenge_answer(event):
     respond()
