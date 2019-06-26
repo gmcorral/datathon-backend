ec0a36471c2ae0ea56e8423bd146dc20307799cd
Guillermo Menéndez <gmenende@amazon.com>
2019-06-26 10:25:57 +0200

    Added accept / reject methods

--- datathon-microservice.py
+++ datathon-microservice.py
@@ -83,12 +83,12 @@ def lambda_handler(event, context):
             return get_answers()
     
     elif operation == 'POST':
-        if resource == '/answers/{teamId}/{challengeId}':
+        if resource == '/answers/{teamId}/{challengeid}':
             logger.info('post_answer_approve')
             return post_answer_approve(event['pathParameters'])
 
     elif operation == 'DELETE':
-        if resource == '/answers/{teamId}/{challengeId}':
+        if resource == '/answers/{teamId}/{challengeid}':
             logger.info('post_answer_reject')
             return post_answer_reject(event['pathParameters'])
     
@@ -114,7 +114,7 @@ def lambda_handler(event, context):
     elif operation == 'POST':
         if resource == '/challenges/{id}/answer':
             logger.info('post_challenge_answer')
-            return post_challenge_answer(event['pathParameters'], username)
+            return post_challenge_answer(event['pathParameters'], event['body'], username)
         else: 
             logger.error('No matching POST resource')
             return respond(err='Unknown resource "{}"'.format(resource), status=400)
@@ -153,7 +153,7 @@ def get_challenges(username):
     return respond(res=[team_challenges[chId] for chId in sorted(team_challenges, key=lambda t: t)])
 
 
-def get_challenge_hint(params, username):
+def get_challenge_hint(params, teamId):
 
     if not 'id' in params:
         return respond(err='Missing challenge ID on path parameters', status=400)
@@ -170,7 +170,7 @@ def get_challenge_hint(params, username):
     try:
         answers_table.update_item(
             Key={
-                'teamId': username,
+                'teamId': teamId,
                 'challengeId': challengeId
             },
             UpdateExpression='SET hinted = :true, #statusAttr = :unanswered, points = :points',
@@ -189,6 +189,8 @@ def get_challenge_hint(params, username):
     except Exception:
         return respond(err='Hint already requested or question already answered', status=400)
     
+    logger.info("Team " + teamId + " got hint for challenge " + challengeId)
+
     return respond(res=
         {
             "hint" : hint
@@ -235,13 +237,84 @@ def get_answers():
     return respond(res=answers)
     
 
-def post_challenge_answer(params, username):
+def post_challenge_answer(params, body, teamId):
+
+    if not 'id' in params:
+        return respond(err='Missing challenge ID on path parameters', status=400)
+    
+    if not 'answer' in body:
+        return respond(err='Missing answer on message body', status=400)
+    
+    answer = body['answer']
+    challengeId = params['id']
+    challenges = get_challenge_cache()
+    if not challengeId in challenges:
+        return respond(err='Challenge ID not found', status=404)
+    
+    points = challenges[challengeId]['points']
+
+    # record answer for challenge
+    try:
+        answers_table.update_item(
+            Key={
+                'teamId': teamId,
+                'challengeId': challengeId
+            },
+            UpdateExpression='SET #statusAttr = :answered, points = :points, answer = :answer',
+            ConditionExpression='attribute_not_exists(#statusAttr) or #statusAttr = :unanswered',
+            ExpressionAttributeNames={
+                '#statusAttr': 'status'
+            },
+            ExpressionAttributeValues={
+                ':answered': 'ANSWERED',
+                ':unanswered': 'UNANSWERED',
+                ':points': int(points),
+                ':answer': answer
+            }
+        )
+
+    except Exception as ex:
+        logger.info(ex)
+        return respond(err='Question already answered', status=400)
+    
+    logger.info("Team " + teamId + " answered challenge " + challengeId)
+
     return respond()
+
 
 def post_answer_approve(params):
+
+    if not 'teamId' in params or not 'challengeid' in params:
+        return respond(err='Missing team ID or challenge ID on path parameters', status=400)
+    
+    teamId = params['teamId']
+    challengeId = params['challengeid']
+
+    points = update_approved_status(teamId, challengeId, 'APPROVED')
+    if points < 0:
+        return respond(err='Trying to approve an answer not in ANSWERED status', status=400)
+
+    if not update_game_points(teamId, points):
+        return respond(err='Error updating team ' + teamId + ' with ' + points + ' points', status=500)
+    
+    logger.info("Team " + teamId + " got " + str(points) + " points for challenge " + challengeId)
+
     return respond()
 
+
 def post_answer_reject(params):
+
+    if not 'teamId' in params or not 'challengeid' in params:
+        return respond(err='Missing team ID or challenge ID on path parameters', status=400)
+    
+    teamId = params['teamId']
+    challengeId = params['challengeid']
+
+    if update_approved_status(teamId, challengeId, 'REJECTED') < 0:
+        return respond(err='Trying to reject an answer not in ANSWERED status', status=400)
+    
+    logger.info("Team " + teamId + " got answer for challenge " + challengeId + " rejected")
+
     return respond()
 
 
@@ -286,6 +359,7 @@ def get_challenge_cache():
 #######################
 # DB helper functions
 
+
 def scan_leaderboard(startKey=None):
     if startKey:
         return teams_table.scan(
@@ -296,6 +370,7 @@ def scan_leaderboard(startKey=None):
         return teams_table.scan(
             Select='ALL_ATTRIBUTES'
         )
+
 
 def scan_challenges(startKey=None):
     if startKey:
@@ -308,6 +383,7 @@ def scan_challenges(startKey=None):
             Select='ALL_ATTRIBUTES'
         )
     
+
 def query_answers_by_team(teamId, startKey=None):
     if startKey:
         return answers_table.query(
@@ -320,6 +396,7 @@ def query_answers_by_team(teamId, startKey=None):
             Select='ALL_ATTRIBUTES',
             KeyConditionExpression=Key('teamId').eq(teamId)
         )
+
 
 def query_answers_by_status(status, startKey=None):
     if startKey:
@@ -341,3 +418,60 @@ def query_answers_by_status(status, startKey=None):
             ExpressionAttributeNames={'#st': 'status'},
             ExpressionAttributeValues={ ":stVal": status}
         )
+
+
+def update_game_points(teamId, points):
+
+    # update team points
+    try:
+        teams_table.update_item(
+            Key={
+                'teamId': teamId
+            },
+            UpdateExpression='ADD gamePoints :points',
+            ExpressionAttributeValues={
+                ':points': points
+            }
+        )
+
+    except Exception as ex:
+        logger.error("Trying to update team " + teamId + " with " + points + " points: " + str(ex))
+        return False
+    
+    return True
+
+
+def update_approved_status(teamId, challengeId, status):
+
+    # mark challenge for team as hinted
+    try:
+        response = answers_table.update_item(
+            Key={
+                'teamId': teamId,
+                'challengeId': challengeId
+            },
+            UpdateExpression='SET #statusAttr = :newstatus',
+            ConditionExpression='#statusAttr = :answered',
+            ExpressionAttributeNames={
+                '#statusAttr': 'status'
+            },
+            ExpressionAttributeValues={
+                ':answered': 'ANSWERED',
+                ':newstatus': status
+            },
+            ReturnValues='ALL_NEW'
+        )
+
+        if not 'Attributes' in response or not 'points' in response['Attributes']:
+            return -1
+        points = response['Attributes']['points']
+
+        # hinted answers get half points
+        if 'hinted' in response['Attributes'] and response['Attributes']['hinted'] == 'true':
+            points /= 2
+
+    except Exception as ex:
+        logger.error("Trying to update team " + teamId + " answer for challenge " + challengeId + " with status " + status + ": " + str(ex))
+        return -1
+    
+    return points
